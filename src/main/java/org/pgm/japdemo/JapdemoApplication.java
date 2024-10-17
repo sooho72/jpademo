0768fba22e9943768a08d3077bce45e3f0d3ef25
sifter0926 <sifter0926@gmail.com>
2024-10-17 13:57:33 +0900

    jpademo init

---
+++ src/main/java/org/pgm/japdemo/JapdemoApplication.java
@@ -0,0 +1,13 @@
+package org.pgm.japdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapdemoApplication.class, args);
+    }
+
+}
