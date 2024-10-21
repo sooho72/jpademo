4382a6dde0ab540322d6f4a38cff60a7e4d798b3
sifter0926 <sifter0926@gmail.com>
2024-10-21 11:26:28 +0900

    crud 완성

--- src/main/java/org/pgm/japdemo/controller/BoardController.java
+++ src/main/java/org/pgm/japdemo/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Log4j2
@@ -30,6 +31,24 @@ public class BoardController {
     public String registerPost(Board board) {
         log.info("controller registerPost");
         boardService.saveBoard(board);
+        return "redirect:/board/list";
+    }
+    @GetMapping({"/read","/modify"})
+    public void read(Long bno, Model model) {
+        log.info("controller read"+bno);
+        model.addAttribute("dto", boardService.getBoard(bno));
+    }
+    @PostMapping("/modify")
+    public String modify(Board board,RedirectAttributes redirectAttributes) {
+        log.info("controller modify"+board);
+        boardService.updateBoard(board);
+        redirectAttributes.addAttribute("bno", board.getBno());
+        return "redirect:/board/read";
+    }
+    @PostMapping("/remove")
+    public String remove(Board board) {
+        log.info("controller remove"+board);
+        boardService.deleteBoard(board.getBno());
         return "redirect:/board/list";
     }
 }
