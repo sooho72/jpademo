6b1dffc781f7255dcb24442e045f5be522d64d3a
sifter0926 <sifter0926@gmail.com>
2024-10-22 10:30:49 +0900

    querydsl 코드작성

---
+++ src/main/java/org/pgm/japdemo/repository/search/BoardSearch.java
@@ -0,0 +1,9 @@
+package org.pgm.japdemo.repository.search;
+
+import org.pgm.japdemo.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BoardSearch {
+    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
+}
